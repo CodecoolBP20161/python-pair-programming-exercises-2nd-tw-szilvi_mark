0b3886695d858ad3f6228cf626e648b936d38481
makaimark <makaimark@gmail.com>
2016-05-30 13:36:19 +0200

    ok

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,10 +1,16 @@
-def palindrome(str):
-    return
+def palindrome(str1):
+    str1 = str1.lower()
+    str1 = str1.replace(" ", "")
+    print(str1[::-1])
+    print(str1)
+    if str1 == str1[::-1]:
+        return True
+    else:
+        return False
 
 
 def main():
-    return
-
+    print(palindrome("Anna"))
 
 if __name__ == '__main__':
     main()
