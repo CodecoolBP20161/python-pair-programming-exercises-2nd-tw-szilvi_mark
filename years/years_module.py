cffa192c80fc109d2728fa384db3d4af045904d7
makaimark <makaimark@gmail.com>
2016-05-30 11:47:32 +0200

    finish

--- years/years_module.py
+++ years/years_module.py
@@ -2,11 +2,14 @@ import datetime
 
 
 def years(age):
-    return
+    return_age = 100-int(age)
+    return (return_age + 2016)
 
 
 def main():
-    return
+    input_age = input("write here your age:")
+    name = input("Write here your name:")
+    print(years(input_age))
 
 
 if __name__ == '__main__':
