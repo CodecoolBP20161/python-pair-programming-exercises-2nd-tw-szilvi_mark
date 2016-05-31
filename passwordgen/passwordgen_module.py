23d41acceecffa5ee0c396669dbb0ec10bd70b98
makaimark <makaimark@gmail.com>
2016-05-31 09:37:59 +0200

    extra 1.0

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,18 +1,30 @@
 import random
 import string
 
+strong_or_weak = input("Strong or weak password would you like to choose?")
+
 
 def passwordgen():
-    list_password = []
-    random_lenght = random.randint(8, 14)
-    for i in range(0, random_lenght+1):
-        random_char = random.randint(33, 127)
-        list_password.append(str(chr(random_char)))
-    return str(list_password)
+    global strong_or_weak
+    for_weak_password = ['hahika', 'hihike', 'ilovecheese']
+    if strong_or_weak == "strong":
+        list_password = []
+        random_lenght = random.randint(8, 14)
+        for i in range(0, random_lenght+1):
+            random_char = random.randint(33, 127)
+            list_password.append(str(chr(random_char)))
+        return str(list_password)
+    elif strong_or_weak == "weak":
+        return random.choice(for_weak_password)
 
 
 def main():
-    print(passwordgen())
+    if strong_or_weak == "strong":
+        print("Your strong password is:")
+    elif strong_or_weak == "weak":
+        print("Your weak password is:")
+    result = passwordgen()
+    print(result.strip("[]"))
 
 
 if __name__ == '__main__':
